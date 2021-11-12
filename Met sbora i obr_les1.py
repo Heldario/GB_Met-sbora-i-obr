1260fb60747ea4b011fb8f9714ab48077dcd7336
Heldario <heldaru@yandex.ru>
2021-11-12 18:45:13 +0300

    Добавлена Задача 2 по апи Вконтакте

--- Met sbora i obr_les1.py
+++ Met sbora i obr_les1.py
@@ -2,6 +2,10 @@
 # разобраться как вывести список репозиториев для конкретного пользователя,
 # сохранить JSON-вывод в файле *.json.
 
+print('======================')
+print('Задача 1')
+print('======================')
+
 import requests
 import json
 from pprint import pprint
@@ -25,3 +29,26 @@ for i in range(n):
 
 with open('les1_to_json.json', 'w') as f:
     json.dump(j_data, f)
+
+# Задача 2. Изучить список открытых API (https://www.programmableweb.com/category/all/apis).
+# Найти среди них любое, требующее авторизацию (любого типа). Выполнить запросы к нему, пройдя авторизацию.
+# Ответ сервера записать в файл.
+# Если нет желания заморачиваться с поиском, возьмите API вконтакте (https://vk.com/dev/first_guide).
+# Сделайте запрос, чтобы получить список всех сообществ на которые вы подписаны.
+
+print('======================')
+print('Задача 2')
+print('======================')
+
+token_vk = 'xxx'
+method_vk = 'groups.get'
+url_vk = f'https://api.vk.com/method/{method_vk}?v=5.52&access_token={token_vk}&v=5.131'
+
+response_vk = requests.get(url_vk)
+j_data_vk = response_vk.json()
+
+print(f'There are some groups:')
+
+print(j_data_vk['response']['items'])
+# С выводом названий сообществ не стал заморачиваться.
+# Делается вызовом метода groups.getById с передачей параметру group_ids полученных items
